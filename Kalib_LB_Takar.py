10c37aba8e3775116df607f37bc4f3dfa79f87a9
alif4addar <alifqaddar@gmail.com>
2025-07-20 09:21:17 +0700

    Update Kalib_LB_Takar.py

--- Kalib_LB_Takar.py
+++ Kalib_LB_Takar.py
@@ -9,9 +9,7 @@ from pathlib import Path
 
 
 
-css_file = Path(__file__).parent / "gaya.css"
-with open(css_file) as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 
 st.set_page_config(
     page_title="Aplikasi Kalibrasi Volume",
