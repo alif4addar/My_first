37af1088b5a88173bd11ac0bb9f2139f99df8f9c
alif4addar <alifqaddar@gmail.com>
2025-07-20 20:01:14 +0700

    Update kalib.py

--- kalib.py
+++ kalib.py
@@ -14,7 +14,7 @@ st.set_page_config(
     page_title="Aplikasi Kalibrasi Volume",
     page_icon="📖",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapsed"
 )
 
 if "rows" not in st.session_state:
